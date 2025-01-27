01900fe3ff551912d3dd528d8424d955fa049058
Kirsten-Holm <dj4kbox@gmail.com>
2025-01-27 19:13:18 +0100

    Added report function and dict to list of dicts function

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 def main():
     bookPath = "books/frankenstein.txt"
-    text = getBookText(bookPath)
-    wordCount = countWords(text)
-    charCount = getCharacterCounts(text)
-    print(charCount)
+    bookReport(bookPath)
+
+    
 
 
 
@@ -11,11 +10,18 @@ def countWords(bookString):
     bookList = bookString.split()
     return len(bookList)
 
-
-
 def getBookText(path):
     with open(path) as f:
         return f.read()
+    
+def sortOn(dict):
+    return dict["count"]
+
+def getListDict(dict):
+    charDictList = []
+    for char,count in dict.items():
+        charDictList.append({"char": char, "count":count})
+    return charDictList
 
 def getCharacterCounts(BookString):
     BookString = BookString.lower()
@@ -27,7 +33,19 @@ def getCharacterCounts(BookString):
             charDict[char] +=1
     return charDict
 
-
+def bookReport(pathToBook):
+    print(f"--- Begin report of {pathToBook} ---")
+    text = getBookText(pathToBook)
+    print(f"{countWords(text)} words found in the document\n\n")
+    charCount = getCharacterCounts(text)
+    charCountList = getListDict(charCount) 
+    charCountList.sort(reverse=True,key=sortOn)
+    for char in charCountList:
+        if char["char"].isalpha():
+            print(f"The '{char["char"]}' character was found {char["count"]} times")
+        else:
+            pass
+    print("\n ---End of Report--- ")
 
 
 
