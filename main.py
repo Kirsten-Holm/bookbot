9568c7d0a57d58eb37e18c9552b6555a616c8e08
Kirsten-Holm <dj4kbox@gmail.com>
2025-01-27 18:07:19 +0100

    Added character count function

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 def main():
     bookPath = "books/frankenstein.txt"
     text = getBookText(bookPath)
-    print(countWords(text))
+    wordCount = countWords(text)
+    charCount = getCharacterCounts(text)
+    print(charCount)
 
 
 
@@ -15,7 +17,15 @@ def getBookText(path):
     with open(path) as f:
         return f.read()
 
-
+def getCharacterCounts(BookString):
+    BookString = BookString.lower()
+    charDict = {}
+    for char in BookString:
+        if char not in charDict:
+            charDict[char] = 1
+        else:
+            charDict[char] +=1
+    return charDict
 
 
 
